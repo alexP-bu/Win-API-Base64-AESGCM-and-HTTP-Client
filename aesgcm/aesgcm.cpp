c2edb31a100e165302932714becb44b5b3b78c4d
alexP-bu <apozin@bu.edu>
2022-11-20 23:35:00 -0500

    finished aes-gcm

--- aesgcm/aesgcm.cpp
+++ aesgcm/aesgcm.cpp
@@ -69,7 +69,6 @@ AESGCM::AESGCM( BYTE key[AES_256_KEY_SIZE]){
 
 void AESGCM::Decrypt(BYTE* nonce, size_t nonceLen, BYTE* data, size_t dataLen, BYTE* macTag, size_t macTagLen){
     NTSTATUS status;
-
     //get buffer size
     ULONG pcbResult;
     status = BCryptDecrypt(hKey, data, dataLen, NULL, nonce, nonceLen, NULL, 0, &pcbResult, 0);
@@ -87,42 +86,77 @@ void AESGCM::Decrypt(BYTE* nonce, size_t nonceLen, BYTE* data, size_t dataLen, B
       return;
     }
 
-    status = BCryptDecrypt(hKey, data, dataLen, NULL, nonce, nonceLen, plaintext, pcbResult, &pcbResult, 0);
+    /*setup the decryption*/
+    BCRYPT_AUTHENTICATED_CIPHER_MODE_INFO auth;
+    BCRYPT_INIT_AUTH_MODE_INFO(auth);
+    auth.pbNonce = nonce;
+    auth.cbNonce = nonceLen;
+    auth.pbAuthData = NULL;
+    auth.cbAuthData = 0;
+    auth.pbTag = macTag;
+    auth.cbTag = macTagLen;
+    auth.pbMacContext = NULL;
+    auth.cbMacContext = 0;
+    auth.cbAAD = 0;
+    auth.cbData = 0;
+    auth.dwFlags = 0;
+    ULONG numBytes = 0;
+    status = BCryptDecrypt(hKey, data, dataLen, &auth, nonce, nonceLen, plaintext, pcbResult, &numBytes, 0);
 		if(!NT_SUCCESS(status)){
 			printf("[!] Error decrypting data: %x", status);
 			Cleanup();
 			return;
 		}
+    return;
 }
 
+/*https://stackoverflow.com/questions/30720414/how-to-chain-bcryptencrypt-and-bcryptdecrypt-calls-using-aes-in-gcm-mode*/
 void AESGCM::Encrypt(BYTE* nonce, size_t nonceLen, BYTE* data, size_t dataLen){
+   //malloc the tag
+   tag = (BYTE*)malloc(authTagLengths.dwMaxLength);
+   //setup auth 
+   BCRYPT_AUTHENTICATED_CIPHER_MODE_INFO auth;
+   BCRYPT_INIT_AUTH_MODE_INFO(auth);
+   auth.pbNonce = nonce;
+   auth.cbNonce = nonceLen;
+   auth.pbAuthData = NULL;
+   auth.cbAuthData = 0;
+   auth.pbTag = tag;
+   auth.cbTag = authTagLengths.dwMaxLength;
+   auth.pbMacContext = NULL;
+   auth.cbMacContext = 0;
+   auth.cbAAD = 0;
+   auth.dwFlags = 0;
+
    //get length for output buffer
    NTSTATUS status;
-
    ULONG pcbResult;
-   status = BCryptEncrypt(hKey, data, dataLen, NULL, nonce, nonceLen, NULL, 0, &pcbResult, 0);
+   status = BCryptEncrypt(hKey, data, dataLen, &auth, nonce, nonceLen, NULL, 0, &pcbResult, 0);
    if (!NT_SUCCESS(status)){
     printf("[!] Error getting buffer size!");
     Cleanup();
     return;
    }
-    
+
    //allocate buffer for ciphertext
+   ptBufferSize = pcbResult;
    ciphertext = (BYTE*)malloc(pcbResult);
    if(!ciphertext){
-    printf("[!] Error with malloc!");
+    printf("[!] Error allocating memory!");
     Cleanup();
     return;
    }
 
+   ULONG numBytes = 0;
    //encrypt data
-   ULONG bytesDone;
-   status = BCryptEncrypt(hKey, data, dataLen, NULL, nonce, nonceLen, ciphertext, pcbResult, &bytesDone, 0);
+   status = BCryptEncrypt(hKey, data, dataLen, &auth, nonce, nonceLen, ciphertext, pcbResult, &numBytes, 0);
    if(!NT_SUCCESS(status)){
     printf( "[!] Error encrypting: %x", status);
     Cleanup();
     return;
    }
+   //success!
+   return;
 }
 
 void AESGCM::Cleanup(){
